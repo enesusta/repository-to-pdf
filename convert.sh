aa849ffbfdd6eb3a9536f97e7e64c7004af61c6c
Enes Usta <enesusta@email.com>
2019-12-17 22:23:10 +0300

    some styles added

--- convert.sh
+++ convert.sh
@@ -1,22 +1,71 @@
 #!/bin/bash
 
-echo "Output file name: "
-read name
+create() {
+    pdflatex $1
+}
 
-cat < src/template.tex > "$name.tex"
+printf "%s\n\n" "$(cat <src/ascii)"
+
+languages='Java JavaScript Bash C'
+choice=0
+
+PS3='What is the language of your source code ? : '
+
+select language in $languages
+do
+    choice=$language
+    break
+done
+
+echo $choice
+
+read -p 'The name of output file: ' name
 
 shopt -s globstar
 
-for i in **/*.java;
-do	
-	if [ -f "$i" ]; then
-	printf "Filename: %s\n" "${i##*/}"
-	printf "Path: %s\n" "`readlink -f $i`"
-	printf "%s\n %s\n %s\n" "\\begin{lstlisting}[,caption={}]" "$(<$i)" "\\end{lstlisting}" >> "$name.tex"
-	fi
-done
+case $choice in
+    Java)
+        cat < src/java.tex > "$name.tex"
+        for i in **/*.java;
+        do
+            if [ -f "$i" ]; then
+                printf "Filename: %s\n" "${i##*/}"
+                #printf "Path: %s\n" "`readlink -f $i`"
+                #printf "%s\n %s\n %s\n" "\\begin{lstlisting}[,caption={}]" "$(<$i)" "\\end{lstlisting}" >> "$name.tex"
+            fi
+        done
+        echo "\\end{document}" >> "$name.tex"
+        create "$name.tex"
+    ;;
+    JavaScript)
+        cat < src/javascript.tex > "$name.tex"
+        for i in **/*.js;
+        do
+            if [ -f "$i" ]; then
+                printf "Filename: %s\n" "${i##*/}"
+                #printf "Path: %s\n" "`readlink -f $i`"
+                #printf "%s\n %s\n %s\n" "\\begin{lstlisting}[,caption={}]" "$(<$i)" "\\end{lstlisting}" >> "$name.tex"
+            fi
+        done
+        
+        echo "\\end{document}" >> "$name.tex"
+        create "$name.tex"
+    ;;
+    C)
+        cat < src/c.tex > "$name.tex"
+        for i in **/*.c;
+        do
+            if [ -f "$i" ]; then
+                printf "Filename: %s\n" "${i##*/}"
+                #printf "Path: %s\n" "`readlink -f $i`"
+                #printf "%s\n %s\n %s\n" "\\begin{lstlisting}[,caption={}]" "$(<$i)" "\\end{lstlisting}" >> "$name.tex"
+            fi
+        done
+        
+        echo "\\end{document}" >> "$name.tex"
+        create "$name.tex"
+    ;;
+    *)
+esac
 
-echo "\\end{document}" >> "$name.tex"
 
-
-pdflatex "$name.tex"
